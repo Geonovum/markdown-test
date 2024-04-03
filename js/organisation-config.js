416053dffb9fad09c00f1605b6f45bf7bcf5d70e
GN-BOT-WORKFLOW <99737370+GN-BOT-WORKFLOW@users.noreply.github.com>
2024-04-03 14:42:46 +0200

    Update organisation-config.js

--- js/organisation-config.js
+++ js/organisation-config.js
@@ -2,6 +2,9 @@ var organisationConfig =
 {
   nl_organisationName: "Geonovum",
   nl_organisationPublishURL: "https://docs.geostandaarden.nl/",
+  latestVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName"],
+  thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "specStatus", "-", "specType", "-", "shortName", "-", "publishDate"],
+  prevVersion: ["nl_organisationPublishURL", "pubDomain", "/", "previousMaturity", "-", "specType", "-", "shortName", "-", "previousPublishDate"],
   postProcess: [
     window.respecMermaid.createFigures
   ],
